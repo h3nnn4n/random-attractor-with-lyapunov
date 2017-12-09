5247baa29cf093df0eccee7b74230aae4c45c2dc
h3nnn4n <uber.renan@gmail.com>
2017-12-09 10:18:03 -0200

    Small cleanup

---
+++ clean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -v *{png,jpg,ppm,txt} > /dev/null 2>/dev/null || true
+rm -v feh* > /dev/null 2> /dev/null || true
--- convert.sh
+++ convert.sh
@@ -1,7 +1,8 @@
-for x in `ls -w 1 *.ppm`; 
+#!/bin/bash
+
+for x in `ls -w 1 *.ppm`;
 do
 	j=`echo $x | tr -d .pm`
 	convert $x w_$j.png
 	echo $x to w_$j.png
 done
-
